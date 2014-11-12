39b16cf929ab3f6c71fb9ceb47b18bb1bce728eb
jkol36 <jonathankolman@gmail.com>
2014-11-11 20:41:27 -0500

    updates

--- js/selectize.js
+++ js/selectize.js
@@ -13,7 +13,28 @@
  *
  * @author Brian Reavis <brian@thirdroute.com>
  */
+$(function(){
+      $("#first-typed").typed({
+        strings: ["Cake Topper", "Cufflinks", "Wedding Gift", "Necklaces", "Wine Charms", "Confetti", "Table Centerpieces"],
+        typeSpeed: 80,
+        showCursor: false,
+        backDelay: 5000,
+        startDelay: 500,
+        loop: true
 
+      });
+    });
+    $(function(){
+      $("#second-typed").typed({
+        strings: ["your Wedding", "Groomsmen", "Your Fiancé", "Bridesmaids", "a Dinner Party", "New Years Eve", "a Baby Shower"],
+        typeSpeed: 80,
+        showCursor: false,
+        backDelay: 5000,
+        startDelay: 3000,
+        loop: true
+
+      });
+    });
 (function(root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		define('sifter', factory);
